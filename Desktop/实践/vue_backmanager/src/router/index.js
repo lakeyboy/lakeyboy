8dcfedaf71f8b093b2dcce4f918cf95c0db8bb4b
zhonglive <innovateandworld@163.com>
2020-10-28 19:56:53 +0800

    登录界面

--- Desktop/实践/vue_backmanager/src/router/index.js
+++ Desktop/实践/vue_backmanager/src/router/index.js
@@ -1,23 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+
 
 Vue.use(VueRouter)
 
+// 配置路由映射关系
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  //配置重定向
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login }
 ]
 
 const router = new VueRouter({
