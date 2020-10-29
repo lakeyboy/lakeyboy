ab8dbf489369168cc3769b570033bc98180598fb
zhonglive <innovateandworld@163.com>
2020-10-29 15:24:03 +0800

    登录

--- Desktop/实践/vue_backmanager/src/components/Login.vue
+++ Desktop/实践/vue_backmanager/src/components/Login.vue
@@ -1,13 +1,52 @@
 <template>
   <div class="login_container">
     <div class="login_box">
-        
+      <!-- 头像区域 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+      <!-- 表单区域 -->
+      <!-- 绑定 数据对象 -->
+      <el-form :model="loginForm" label-width="0px" class="login_form">
+        <!-- 用户名 -->
+        <el-form-item>
+          <el-input
+            v-model="loginForm.username"
+            prefix-icon="iconfont icon-iconfonticon-yonghu"
+          ></el-input>
+        </el-form-item>
+
+        <!-- 密码 -->
+        <el-form-item>
+          <el-input
+            v-model="loginForm.password"
+            type="password"
+            prefix-icon="iconfont icon-iconfonticon-mim"
+          ></el-input>
+        </el-form-item>
+
+        <!-- 按钮 -->
+        <el-form-item class="btns">
+          <el-button type="primary">主要按钮</el-button>
+          <el-button type="info">信息按钮</el-button>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      // 这是登陆表单的数据绑定对象
+      loginForm: {
+        username: '11',
+        password: '222',
+      },
+    }
+  },
+}
 </script>
 
 <style lang="less" scoped>
@@ -25,5 +64,36 @@ export default {};
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
+
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+    }
+  }
+
+  .login_form {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+  }
+  .btns {
+    display: flex;
+    justify-content: flex-end;
+  }
 }
 </style>
