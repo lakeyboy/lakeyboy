240331650f94781827bcebdb17c824c661082738
zhonglive <innovateandworld@163.com>
2020-11-07 22:59:31 +0800

    用户数据列表

--- Desktop/实践/vue_backmanager/src/components/user/Users.vue
+++ Desktop/实践/vue_backmanager/src/components/user/Users.vue
@@ -23,12 +23,117 @@
           <el-button type="primary">添加用户</el-button>
         </el-col>
       </el-row>
+      <!-- 用户列表区域 -->
+      <el-table :data="userlist" border stripe>
+        <!-- 每个column  表示一列 -->
+        <el-table-column type="index" label="#"></el-table-column>
+        <el-table-column label="姓名" prop="username"> </el-table-column>
+
+        <el-table-column label="邮箱" prop="email"> </el-table-column>
+
+        <el-table-column label="电话" prop="mobile"> </el-table-column>
+
+        <el-table-column label="角色" prop="role_name"> </el-table-column>
+
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <!-- cope.row 表示这一列的数据 -->
+            <!-- 向数据绑定 -->
+            <!-- {{scope.row}} -->
+            <el-switch v-model="scope.row.mg_state"> </el-switch>
+          </template>
+        </el-table-column>
+
+        <el-table-column label="操作" width="180px">
+          <template>
+            <!-- 修改 -->
+            <el-button
+              type="primary"
+              icon="el-icon-edit"
+              size="mini"
+            ></el-button>
+            <!-- 删除 -->
+            <el-button
+              type="danger"
+              icon="el-icon-delete"
+              size="mini"
+            ></el-button>
+            <!-- 分配角色 -->
+            <el-tooltip
+              effect="dark"
+              content="分配角色"
+              placement="top"
+              :enterable="false"
+            >
+              <el-button
+                type="warning"
+                icon="el-icon-setting"
+                size="mini"
+              ></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页区域 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[1, 2, 5, 10]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      >
+      </el-pagination>
     </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    //获取用户列表的参数对象
+    return {
+      queryInfo: {
+        query: '',
+        // 当前页数
+        pagenum: 1,
+        // 显示个数
+        pagesize: 2,
+      },
+      userlist: [],
+      total: 0,
+    }
+  },
+  // 生命周期函数
+  created() {
+    // 获取用户列表数据
+    this.getUserList()
+  },
+  methods: {
+    //由于返回时promise对象，所以需要使用  await  async 优化异步操作
+    async getUserList() {
+      // 解构数据对象
+      const { data: res } = await this.$http.get('users', {
+        params: this.queryInfo,
+      })
+      if (res.meta.status !== 200)
+        return this.$message.error('获取用户列表失败!')
+      this.userlist = res.data.users
+      this.total = res.data.total
+    },
+    //监听 pagesize 改变的事件
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getUserList()
+    },
+    //监听  页码值 改变的事件
+    handleCurrentChange(newPage) {
+      this.queryInfo.pagenum = newPage
+      this.getUserList()
+    },
+  },
+}
 </script>
 
 <style lang="less" scoped>
--- Desktop/实践/vue_backmanager/src/plugins/element.js
+++ Desktop/实践/vue_backmanager/src/plugins/element.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 //中间有个横杠的组件  去掉后需要首字母大写
 import {
-    Button, Form, FormItem, Input, Message, container, header, aside, main,
-    menu, submenu, MenuItem, breadcrumb, BreadcrumbItem, card, Row, Col
+    Button, Form, FormItem, Input, Message, Container, Header, Aside, Main,
+    Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn,
+    Switch, Tooltip, Pagination
 } from 'element-ui'
 
 //全局注册
@@ -11,23 +12,30 @@ Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
 
-Vue.use(container)
-Vue.use(header)
-Vue.use(aside)
-Vue.use(main)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
 
-Vue.use(menu)
-Vue.use(submenu)
+Vue.use(Menu)
+Vue.use(Submenu)
 Vue.use(MenuItem)
 
-Vue.use(breadcrumb)
+Vue.use(Breadcrumb)
 Vue.use(BreadcrumbItem)
 
-Vue.use(card)
+Vue.use(Card)
 
 Vue.use(Row)
 Vue.use(Col)
 
+Vue.use(Table)
+Vue.use(TableColumn)
+
+Vue.use(Switch)
+Vue.use(Tooltip)
+
+Vue.use(Pagination)
 
 //需要挂载到组件原型上面 这样才可以在全剧中使用
 Vue.prototype.$message = Message;
