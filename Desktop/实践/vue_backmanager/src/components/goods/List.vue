8aecac97b6f07cc7625f0b507f081b18cd944a80
zhonglive <innovateandworld@163.com>
2020-11-19 23:49:53 +0800

    商品列表

---
+++ Desktop/实践/vue_backmanager/src/components/goods/List.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <!-- 面包屑导航区 -->
+    <!-- 样式可以应用于全局 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图 -->
+    <el-card>
+      <!-- 搜索框 -->
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-input placeholder="请输入内容">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary">添加商品</el-button>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      //查询参数对象
+      queryInfo: {
+        query: '',
+        pagenum: '1',
+        pagesize: '10',
+      },
+      //商品列表数据
+      goodslist: [],
+      //总数据条数
+      total: 0,
+    }
+  },
+  created() {
+    this.getGoodList()
+  },
+  methods: {
+    //   根据分页获取对应的数据
+    async getGoodList() {
+      const { data: res } = await this.$http.get('goods', {
+        params: this.queryInfo,
+      })
+
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取参数列表失败！')
+      }
+
+      this.$message.success('获取参数列表成功！')
+      this.goodslist = res.data.goods
+      this.total = res.data.total
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+</style>
